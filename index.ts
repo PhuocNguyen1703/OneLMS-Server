d79d1561e3b638087ac0cbc7fd9571a70765d0a8
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2024-11-27 02:59:10 +0700

    connect to DB

--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+const connectDB = async () => {
+  await mongoose.connect(`${process.env.MONGO_DB}`);
+};
+
+connectDB()
+  .then(() => console.log("Connected successfully to mongoDB"))
+  .then(() => bootServer())
+  .catch((error) => {
+    console.log(error);
+  });
 
 const bootServer = () => {
   const app = express();
 
   app.listen(process.env.PORT || 5000, () => {
     console.log("Server is running");
-    
-  })
+  });
 };
